2eb7267a671597b3831882803b0ca7f22a023958
Atosh-singh <atoshsingh7536@gmail.com>
2022-10-12 11:49:45 +0530

    parsing request body

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
-const http = require("http");
+const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  console.log(req);
+
+
+
+const server =http.createServer((req,res)=>{
+const url = req.url;
+const method =req.method;
+if(url === '/'){
+  res.write('<html>');
+  res.write('<head><title>Enter Message</title><head>');
+  res.write('<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>');
+ res.write ('</html>');
+ return res.end();
+}
+if(url=== '/message' && method ==='POST'){
+ const body=[];
+  req.on('data',function (chunk){
+    console.log(chunk);
+    body.push(chunk)
+  });
+  req.on('end',()=>{
+    const parsedBody = Buffer.concat(body).toString();
+    console.log(parsedBody);
+  });
+  fs.writeFileSync('message.text', 'Dummy');
+  res.statusCode =302;
+  res.setHeader('Location', '/');
+  return res.end();
+}
+
+
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>Hello Panku</title></head>');
+res.write('<body></body>');
+res.write('</html>');
+res.end();
+
 });
 
 server.listen(3000);
+
+
